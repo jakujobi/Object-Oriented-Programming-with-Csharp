db22ba04697994cbfdd725d36d5bde4c3972d533
jakujobi <akujobi365@gmail.com>
2024-01-17 14:38:20 -0600

    Update Test1.AssemblyInfo.cs version number

--- Test1/Test1/obj/Debug/net8.0/Test1.AssemblyInfo.cs
+++ Test1/Test1/obj/Debug/net8.0/Test1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34bedbf05adb6d98f9b80be949e85e9e409417bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
